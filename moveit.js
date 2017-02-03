d722953165a2bf1d46e01aa3137437041f9e97ae
Ramin Siahcheshmi <ramin.siahcheshmi@meltwater.com>
2017-02-04 00:24:10 +0100

    add polygon length

--- moveit.js
+++ moveit.js
@@ -9,6 +9,57 @@ Moveit.prototype.getLength = function() {
             d;
         if (tagName === 'path') {
             d = this.el.getTotalLength();
+        }else if(tagName === 'polygon') {
+        	var points = this.el.getAttribute('points');
+                points = points.split(" ");
+            var x1 = null, x2, y1 = null, y2 , d = 0, x3, y3;
+            for(var i = 0; i < points.length; i++){
+                var coords = points[i].split(",");
+                if(x1 == null && y1 == null){
+
+                    if(/(\r\n|\n|\r)/gm.test(coords[0])){
+                        coords[0] = coords[0].replace(/(\r\n|\n|\r)/gm,"");
+                        coords[0] = coords[0].replace(/\s+/g,"");
+                    }
+
+                    if(/(\r\n|\n|\r)/gm.test(coords[1])){
+                        coords[0] = coords[1].replace(/(\r\n|\n|\r)/gm,"");
+                        coords[0] = coords[1].replace(/\s+/g,"");
+                    }
+
+                    x1 = coords[0];
+                    y1 = coords[1];
+                    x3 = coords[0];
+                    y3 = coords[1];
+
+                }else{
+
+                    if(coords[0] != "" && coords[1] != ""){             
+
+                        if(/(\r\n|\n|\r)/gm.test(coords[0])){
+                            coords[0] = coords[0].replace(/(\r\n|\n|\r)/gm,"");
+                            coords[0] = coords[0].replace(/\s+/g,"");
+                        }
+
+                        if(/(\r\n|\n|\r)/gm.test(coords[1])){
+                            coords[0] = coords[1].replace(/(\r\n|\n|\r)/gm,"");
+                            coords[0] = coords[1].replace(/\s+/g,"");
+                        }
+
+                        x2 = coords[0];
+                        y2 = coords[1];
+
+                        d += Math.sqrt(Math.pow((x2-x1), 2)+Math.pow((y2-y1),2));
+
+                        x1 = x2;
+                        y1 = y2;
+                        if(i == points.length-2){
+                            d += Math.sqrt(Math.pow((x3-x1), 2)+Math.pow((y3-y1),2));
+                        }
+                    }
+                }
+            }
+                
         } else if (tagName === 'circle') {
             d = 2 * Math.PI * parseFloat(this.el.getAttribute('r'));
         } else if (tagName === 'rect') {
